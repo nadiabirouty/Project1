6fa1f262bb5cd0d693e581013f0f0bb91bda21ba
Nadia Birouty <nadiabirouty@berkeley.edu>
2015-09-12 00:31:49 -0700

    Finished Project1

--- Project1/Project1.swift
+++ Project1/Project1.swift
@@ -1,33 +1,109 @@
 func nextCellStates(currentStates: [[Bool]]) -> [[Bool]] {
-    // IMPLEMENT ME
-    // Take a look at https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life for the rules
-    return currentStates
+    let rows = currentStates.indices
+    let columns = currentStates[0].indices
+    var newMatrix = currentStates
+    for row in rows{
+        for col in columns{
+            var neighbors = getNeighbors(currentStates, row: row, column: col)
+            var numAliveNeighbors = 0
+            
+            for neighbor in neighbors{
+                var indexR = neighbor[0]
+                var indexC = neighbor[1]
+                
+                if currentStates[indexR][indexC]{
+                    numAliveNeighbors += 1
+                }
+            }
+            
+            if currentStates[row][col]{
+                if numAliveNeighbors < 2 {
+                    newMatrix[row][col] = false
+                } else if numAliveNeighbors > 3 {
+                    newMatrix[row][col] = false
+                }
+            } else {
+                if numAliveNeighbors == 3 {
+                    newMatrix[row][col] = true
+                }
+            }
+        }
+    }
+    return newMatrix
+}
+//returns a points valid neighors based on whether the neighbors are in the bounds of the matrix. Returns both dead and alive neighbor locations.  Takes in an m by n matrix and a valid point p in this matrix.
+//Boundaries: [0, m] for row and [0, n] for column
+func getNeighbors(matrix: [[Bool]], row: Int, column: Int) -> [[Int]] {
+    let m = matrix.endIndex - 1
+    let n = matrix[0].endIndex - 1
+   
+    var neighbors = [[Int]]()
+    
+    if (row - 1) >= 0{
+        neighbors.append([row-1, column])
+        if (column - 1) >= 0{
+            neighbors.append([row-1, column-1])
+        }
+        if (column + 1) <= n{
+            neighbors.append([row-1, column+1])
+        }
+    }
+    if (row + 1) <= m{
+        neighbors.append([row+1, column])
+        if (column - 1) >= 0{
+            neighbors.append([row+1, column-1])
+        }
+        if (column + 1) <= n{
+            neighbors.append([row+1, column+1])
+        }
+    }
+    if (column - 1) >= 0{
+        neighbors.append([row, column-1])
+    }
+    if (column + 1) <= n{
+        neighbors.append([row, column+1])
+    }
+    return neighbors
 }
 
 class LRUCache<K:Hashable, V> {
     private var capacity: Int
-
+    private var arrayKeyLastUsed: [K]
+    
+    private var structure: Dictionary<K, V>
+    
     init(capacity: Int) {
         self.capacity = capacity
+        self.structure =  [K: V]()
+        self.arrayKeyLastUsed = Array<K>()
     }
 
+    /*
+    *  Get the value of the key if the key exists in the cache, otherwise return nil.
+    */
     func get(k: K) -> V? {
-        // IMPLEMENT ME
-
-        /*
-        *  Get the value of the key if the key exists in the cache, otherwise return nil.
-        */
-
+        self.arrayKeyLastUsed.insert(k, atIndex: 0)
+        for (key, value) in self.structure {
+            if key == k {
+                return value
+            }
+        }
         return nil
     }
-
+    
+    /*
+    * Set or insert the value if the key is not already present.
+    * When the cache reached its capacity, it should invalidate the
+    * least recently used item before inserting a new item.
+    */
     func set(k: K, v: V) {
-        // IMPLEMENT ME
-
-        /*
-        * Set or insert the value if the key is not already present. 
-        * When the cache reached its capacity, it should invalidate the 
-        * least recently used item before inserting a new item.
-        */
+        if capacity > 0{
+            if self.structure.count >= capacity{
+                self.structure.removeValueForKey(arrayKeyLastUsed[0])
+                self.arrayKeyLastUsed.removeAtIndex(0)
+            }
+            structure.updateValue(v, forKey: k)
+            self.arrayKeyLastUsed.append(k)
+        }
     }
 }
--- Project1Tests/Project1Tests.swift
+++ Project1Tests/Project1Tests.swift
@@ -31,6 +31,47 @@ class GOLTests: XCTestCase {
         ]
         XCTAssertEqual(expected, nextStates)
     }
+    
+    func testFluctuating(){
+        var horizontal: [[Bool]] = [
+            [false, true, false],
+            [false, true, false],
+            [false, true, false]
+        ]
+        let nextStates: [[Bool]] = nextCellStates(horizontal)
+        let expected: [[Bool]] = [
+            [false, false, false],
+            [true, true, true],
+            [false, false, false]
+        ]
+        XCTAssertEqual(expected, nextStates)
+    }
+    
+    func testDiamond(){
+        var diamond: [[Bool]] = [
+            [false, true, false],
+            [true, false, true],
+            [false, true, false]
+        ]
+        let nextStates: [[Bool]] = nextCellStates(diamond)
+        XCTAssertEqual(diamond, nextStates)
+    }
+    func testChecker(){
+        var checker: [[Bool]] = [
+            [true, true, false, false],
+            [true, true, false, false],
+            [false, false, true, true],
+            [false, false, true, true]
+        ]
+        let nextStates: [[Bool]] = nextCellStates(checker)
+        let expected: [[Bool]] = [
+            [true, true, false, false],
+            [true, false, false, false],
+            [false, false, false, true],
+            [false, false, true, true]
+        ]
+        XCTAssertEqual(expected, nextStates)
+    }
 }
 
 class LRUCacheTests: XCTestCase {
@@ -52,5 +93,18 @@ class LRUCacheTests: XCTestCase {
         cache.set(4, v: "Swift")
         XCTAssertNil(cache.get(3))
         XCTAssertEqual(cache.get(2)!, "World!")
+    }
+    
+    func testNoCapacity() {
+        let cache = LRUCache<Int, String>(capacity: 0)
+        cache.set(3, v: "Hello")
+        XCTAssertNil(cache.get(3))
+    }
+    
+    func testGetNonExistentKey(){
+        let cache = LRUCache<Int, String>(capacity: 2)
+        cache.set(3, v: "Hello")
+        cache.set(2, v: "World")
+        XCTAssertNil(cache.get(20))
     }
 }
